86d609cc7cf9cd30c4f492dd6e04f0a143089123
Ciaran Downey <me@ciarand.me>
2013-11-17 12:29:39 -0800

    Add stdout logging to bot

--- bot.php
+++ bot.php
@@ -5,6 +5,8 @@ require "vendor/autoload.php";
 use Philip\Philip;
 use Philip\IRC\Response;
 use Guzzle\Http\Client;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Symfony\Component\Yaml\Yaml;
 use Ciarand\PhilipPlugin\MdnPlugin;
 
@@ -12,4 +14,6 @@ $config = Yaml::parse("config.yml");
 
 $bot = new Philip($config);
 $bot->loadPlugin(new MdnPlugin($bot));
+$logger = $bot->getLogger();
+$logger->pushHandler(new StreamHandler("php://stdout", Logger::DEBUG));
 $bot->run();
