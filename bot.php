462e52f5b17a02945b50736a30fe13c995c6bf8c
Ciaran Downey <me@ciarand.me>
2013-11-22 15:50:46 -0800

    Update bot.php to use new plugins

--- bot.php
+++ bot.php
@@ -9,11 +9,15 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Symfony\Component\Yaml\Yaml;
 use Ciarand\PhilipPlugin\MdnPlugin;
+use Ciarand\PhilipPlugin\YesManPlugin;
+use Ciarand\PhilipPlugin\FrenchPhrasePlugin;
 
 $config = Yaml::parse("config.yml");
 
 $bot = new Philip($config);
 $bot->loadPlugin(new MdnPlugin($bot));
+$bot->loadPlugin(new YesManPlugin($bot));
+$bot->loadPlugin(new FrenchPhrasePlugin($bot));
 $logger = $bot->getLogger();
 $logger->pushHandler(new StreamHandler("php://stdout", Logger::DEBUG));
 $bot->run();
